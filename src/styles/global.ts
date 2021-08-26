cda1811b7772b9877d440e74b5d9a769dc316de7
jean_kistenmache <jean_kistenmache@terceiros.sicredi.com.br>
2021-08-26 17:00:13 -0300

    feat: inicio projeto

---
+++ src/styles/global.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  *{
+    margin:0;
+    padding:0;
+    outline:0;
+    box-sizing: border-box;
+  }
+
+  body{
+    display: flex;
+    flex-direction: column;
+  }
+
+  body, input, button {
+    font: 16px Roboto, sans-serif;
+  }
+
+  button{
+    cursor: pointer;
+  }
+`;
